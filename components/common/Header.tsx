7d997b63260af6450e91bca865d3dd8fb2dbc412
Prashant Thakur <prashantthakurji90@gmail.com>
2026-03-17 14:20:40 +0530

    hiding search for specific blog page

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,16 +1,15 @@
+"use client"
+
 import Link from "next/link";
 import { Input } from "../ui/input";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import { Button } from "../ui/button";
+import { usePathname } from "next/navigation";
 
 export default function Header() {
+  const pathname = usePathname();
+  const isBlogsRoute = pathname === "/blogs";
+
   return (
     <nav className="fixed w-full border-b border-neutral-800/20 bg-white z-50">
       <div className="flex flex-row justify-between items-center py-4 px-4 sm:px-0 w-full sm:max-w-[80%] m-auto">
@@ -19,7 +18,9 @@ export default function Header() {
             Agent Blogger
           </h1>
         </Link>
-        <Input placeholder="Search" className="hidden sm:flex w-1/2" />
+        {isBlogsRoute && (
+          <Input placeholder="Search" className="hidden sm:flex w-1/2" />
+        )}
         <SignedOut>
           <div className="flex flex-row gap-4">
             <Button
